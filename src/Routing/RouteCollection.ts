20f97e0aa7b852e7ca1cbb97d6becb38a4885ea0
Robin Panta <hacktivistic@gmail.com>
2021-10-24 01:20:28 +0530

    feat(*): add route colelctions

---
+++ src/Routing/RouteCollection.ts
@@ -0,0 +1,42 @@
+'use strict'
+
+/**
+ * @name RouteCollection
+ * @author Robin Panta <hacktivistic@gmail.com>
+ */
+
+import Route from './Route'
+
+export default class RouteCollection {
+
+    #routes: Route[] = []
+    #namedRoutes: Route[] = []
+
+    add (route: Route) {
+        this.#routes.push(route)
+        return route
+    }
+
+    clear () {
+        this.#routes = []
+        this.#namedRoutes = []
+    }
+
+    getByName (name: string) {
+        return this.#namedRoutes[name]
+    }
+
+    getRoutes () {
+        return this.#routes
+    }
+
+    refreshNames () {
+        this.#namedRoutes = []
+
+        this.#routes.forEach(x => {
+            if (x.getName()) 
+                this.#namedRoutes[x.getName()] = x
+        })
+    }
+
+}
